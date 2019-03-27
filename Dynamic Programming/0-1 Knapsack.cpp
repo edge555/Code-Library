4fa93d3d6a191e2d88ec0f9cea53275e11d05cd5
edge555 <shoaib27@student.sust.edu>
2019-03-27 07:54:20 +0600

    Edited

--- Dynamic Programming/0-1 Knapsack.cpp
+++ Dynamic Programming/0-1 Knapsack.cpp
@@ -1,21 +1,19 @@
-int knapsack
-(
-    int W,int wt[],int val[],int n)
+int knapsack(int W,int wt[],int val[],int n)
+{
+    int i,w,K[n+1][W+1];
+    for (i=0;i<=n;i++)
     {
-        int i,w,K[n+1][W+1];
-        for (i=0;i<=n;i++)
+        for (w=0;w<=W;w++)
         {
-            for (w=0;w<=W;w++)
-            {
-                if (i==0 || w==0)
-                    K[i][w]=0;
-                else if (wt[i-1]<=w)
-                    K[i][w]=max(val[i-1]+K[i-1][w-wt[i-1]],K[i-1][w]);
-                else
-                    K[i][w]=K[i-1][w];
-            }
-     }
-     return K[n][W];
+            if (i==0 || w==0)
+                K[i][w]=0;
+            else if (wt[i-1]<=w)
+                K[i][w]=max(val[i-1]+K[i-1][w-wt[i-1]],K[i-1][w]);
+            else
+                K[i][w]=K[i-1][w];
+        }
+    }
+    return K[n][W];
 }
 int main()
 {
