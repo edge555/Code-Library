e612df0803b106894939198a67466b198f4c5008
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-04-12 16:09:53 +0600

    Edited

--- String/LCS Print with Backtracking.cpp
+++ String/LCS Print with Backtracking.cpp
@@ -1,62 +1,49 @@
-int dp[N][N],t;
-set<string>seq;
-map<string,bool>done[N][N];
-void backtrack(string s1,string s2,int i,int j,string word)
+string a,b,s;
+int dp[N][N];
+int calLcs(int i,int j)
 {
-    if(done[i][j].find(word)!=done[i][j].end())
-        return;
-    if(i==0 || j==0)
-        seq.insert(word);
-    else if(s1[i-1]==s2[j-1])
+    if(i==a.size() || j==b.size())
+        return 0;
+    if(dp[i][j]!=-1)
+        return dp[i][j];
+    int ans=0;
+    if(a[i]==b[j])
+        ans=1+calLcs(i+1,j+1);
+    else
+        ans=max(calLcs(i,j+1),calLcs(i+1,j));
+    return dp[i][j]=ans;
+}
+void print(int i,int j)
+{
+    if(i==a.size() || j==b.size())
     {
-        word=s1[i-1]+word;
-        backtrack(s1,s2,i-1,j-1,word);
+        cout<<s<<endl;
+        return;
     }
-    else if(dp[i][j-1]>dp[i-1][j])
-        backtrack(s1,s2,i,j-1,word);
-    else if(dp[i-1][j]>dp[i][j-1])
-        backtrack(s1,s2,i-1,j,word);
+    if(a[i]==b[j])
+    {
+        s+=a[i];
+        print(i+1,j+1);
+    }
     else
     {
-        backtrack(s1,s2,i,j-1,word);
-        backtrack(s1,s2,i-1,j,word);
+        if(dp[i][j+1]>dp[i+1][j])
+            print(i,j+1);
+        else
+            print(i+1,j);
     }
-    done[i][j][word]=true;
-}
-void lcs(string a,string b)
-{
-    int n=a.length(),i;
-    int m=b.length(),j;
-
-    rep0(i,n+1)
-        dp[i][0]=0;
-    rep0(i,m+1)
-        dp[0][i]=0;
-    rep(i,n)
-    {
-        rep(j,m)
-        {
-            if(a[i-1]==b[j-1])
-                dp[i][j]=dp[i-1][j-1]+1;
-           else
-                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-        }
-    }
-    backtrack(a,b,n,m,"");
 }
 int main()
 {
-    cin>>t;
-    char c_s1[81],c_s2[81];
-    string s1,s2;
+    int t;
+    sf(t);
+    getchar();
     while(t--)
     {
-        seq.clear();
-        scanf("%s %s", c_s1, c_s2);
-        s1=c_s1;
-        s2=c_s2;
-        lcs(s1,s2);
-        for(auto it=seq.begin();it!=seq.end();it++)
-            printf("%s\n",it->c_str());
+        mem(dp,-1);
+        cin>>a>>b;
+        int ans=calLcs(0,0);
+        s="";
+        print(0,0);
     }
 }
