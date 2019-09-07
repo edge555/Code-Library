e1fe17e5c762b9b918a94b13e77b8bf0730cba99
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-09-08 00:03:56 +0600

    Update Chinese Remainder Theorem.cpp

--- Number Theory/Chinese Remainder Theorem.cpp
+++ Number Theory/Chinese Remainder Theorem.cpp
@@ -1,50 +1,28 @@
-int pfn; //number of distinct prime factors
-int pf[N]; //prime factor powers
-int rem[N]; //corresponding remainder
-int pm[N],prime[N];
-void sieve()
-{ }
-int inverse(int x,int p)
+ll p[N],r[N];
+int n;
+void extgcd(ll a,ll b,ll &d,ll &x,ll &y)
 {
-    int q,tmp,a=x,b=p;
-    int a0=1,a1=0,b0=0,b1=1;
-    while(b)
+    if(!b)
+        d=a,x=1,y=0;
+    else
     {
-        q=a/b; tmp=b; b=a-b*q; a=tmp;
-        tmp=b0; b0=a0-b0*q; a0=tmp;
-        tmp=b1; b1=a1-b1*q; a1=tmp;
+        extgcd(b,a%b,d,y,x);
+        y-=x*(a/b);
     }
-    return a0;
 }
-void decompose_mod()
+ll chinese_remainder()
 {
-    int i,p,t=mod;
-    pfn=0;
-    for(i=0;i<pnum && prime[i]<=t;i++)
+    ll M=1;
+    for(int i=0; i<n; i++)
+        M*=p[i];
+    ll ans=0;
+    ll m;
+    ll d,x,y;
+    for(int i=0; i<n; i++)
     {
-        p=prime[i];
-        if(t%p==0)
-        {
-            pf[pfn]=1;
-            while(t%p==0)
-            {
-                t/=p;
-	            pf[pfn]*=p;
-            }
-            pfn++;
-        }
+        m=M/p[i];
+        extgcd(m,p[i],d,x,y);
+        ans=(ans+r[i]*m*x)%M;
     }
-    if(t>1)
-        pf[pfn++]=t;
-}
-int chinese_remainder()
-{
-    int i,m,s=0;
-    rep0(i,pfn)
-    {
-        m=mod/pf[i];
-        pm[i]=(long long)m*inverse(m,pf[i])%mod;
-        s=(s+(long long)pm[i]*rem[i])%mod;
-    }
-    return s;
+    return (ans+M)%M;
 }
