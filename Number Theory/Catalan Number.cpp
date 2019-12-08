41b686ca0175b2d55b28eaec6cdf804ebe608c66
Shoaib Ahmed <shoaib27@student.sust.edu>
2019-12-08 14:36:31 +0600

    Deleted

--- Number Theory/Catalan Number.cpp
+++
@@ -1,9 +0,0 @@
-unsigned long int catalan(unsigned int n)
-{
-    if(n<=1)
-        return 1;
-    unsigned long int res = 0;
-    for(int i=0; i<n; i++)
-        res+=catalan(i)*catalan(n-i-1);
-    return res;
-}
---
+++ Number Theory/Sieve.cpp
@@ -0,0 +1,18 @@
+bool mark[N];
+void sieve()
+{
+    int i,j;
+    memset(mark,true,sizeof(mark));
+    for(i=4;i<N;i+=2)
+        mark[i]=false;
+    for(i=3;i*i<N;i+=2)
+    {
+        if(mark[i])
+        {
+            for(j=i*i;j<N;j+=i*2)
+                mark[j]=false;
+        }
+    }
+    mark[1]=false;
+    //if(mark[n]) -> Prime
+}
