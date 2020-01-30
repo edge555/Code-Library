a3b861a00bb7c037cfeb77b92d0e4cd20c19fa75
Shoaib Ahmed <shoaib27@student.sust.edu>
2020-01-30 21:50:06 +0600

    Added

---
+++ Number Theory/Euler Phi.cpp
@@ -0,0 +1,16 @@
+ll phi(ll n)
+{
+    ll i,ret=n;
+    for(i=2;i*i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            while(n%i==0)
+                n/=i;
+            ret-=ret/i;
+        }
+    }
+    if(n>1)
+        ret-=ret/n;
+    return ret;
+}
