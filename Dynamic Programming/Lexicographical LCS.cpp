332eaa32180e577ead228f7c6ee91bd0e6efe173
Shoaib Ahmed <shoaib27@student.sust.edu>
2020-03-30 21:20:53 +0600

    Edited and Added

--- Dynamic Programming/Lexicographical LCS.cpp
+++ Dynamic Programming/Lexicographical LCS.cpp
@@ -1,23 +1,27 @@
-for(int i=0;i<=m;i++)
+string dp[N][N];
+void lexicographicalLCS(string A,string B)
 {
-    for(int j=0;j<=n;j++)
+    int i,j,m=A.size(),n=B.size();
+    for(i=0;i<=m;i++)
     {
-        if(i==0||j==0)
-            dp[i][j]="";
-        else if(A[i-1]==B[j-1])
-            dp[i][j]=dp[i-1][j-1]+A[i-1];
-        else if(dp[i-1][j].size()>dp[i][j-1].size())
-            dp[i][j]=dp[i-1][j];
-        else if(dp[i-1][j].size()<dp[i][j-1].size())
-            dp[i][j]=dp[i][j-1];
-        else if(dp[i-1][j]<dp[i][j-1])
-            dp[i][j]=dp[i-1][j];
-        /* flip < to > for Lexicographical largest LCS .*/
-        else
-            dp[i][j]=dp[i][j-1];
+        for(j=0;j<=n;j++)
+        {
+            if(i==0||j==0)
+                dp[i][j]="";
+            else if(A[i-1]==B[j-1])
+                dp[i][j]=dp[i-1][j-1]+A[i-1];
+            else if(dp[i-1][j].size()>dp[i][j-1].size())
+                dp[i][j]=dp[i-1][j];
+            else if(dp[i-1][j].size()<dp[i][j-1].size())
+                dp[i][j]=dp[i][j-1];
+            else if(dp[i-1][j]<dp[i][j-1])
+                dp[i][j]=dp[i-1][j];
+            else
+                dp[i][j]=dp[i][j-1];
+        }
     }
+    if(dp[m][n].size()==0)
+        cout<<"No solution"<<endl;
+    else
+        cout<<dp[m][n]<<endl;
 }
-if(dp[m][n].size()==0)
-    puts("No solution");
-else
-    cout<<dp[m][n]<<endl ;
