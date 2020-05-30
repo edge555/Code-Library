ea7cad8c89051b748534a46e649ce2d85a617d2a
Shoaib Ahmed <2010.ahmed.shoaib@gmail.com>
2020-05-30 19:14:09 +0600

    Updated

--- Number Theory/Chinese Remainder Theorem.cpp
+++ Number Theory/Chinese Remainder Theorem.cpp
@@ -1,28 +1,54 @@
-ll p[N],r[N];
-int n;
-void extgcd(ll a,ll b,ll &d,ll &x,ll &y)
+inline ll LCM(ll a,ll b)
 {
-    if(!b)
-        d=a,x=1,y=0;
-    else
-    {
-        extgcd(b,a%b,d,y,x);
-        y-=x*(a/b);
-    }
+    return a/__gcd(a,b)*b;
 }
-ll chinese_remainder()
+inline ll normalize(ll x, ll mod)
 {
-    ll M=1;
-    for(int i=0; i<n; i++)
-        M*=p[i];
-    ll ans=0;
-    ll m;
-    ll d,x,y;
-    for(int i=0; i<n; i++)
+    x%=mod;
+    if(x<0)
+        x+=mod;
+    return x;
+}
+struct GCD_type
+{
+    ll x,y,d;
+};
+GCD_type ex_GCD(ll a,ll b)
+{
+    if(b==0)
+        return {1,0,a};
+    GCD_type pom = ex_GCD(b,a%b);
+    return {pom.y, pom.x - a / b * pom.y , pom.d};
+}
+ll a[N],b[N],r[N],n[N],p[N],ans,lcm;
+int main()
+{
+    int i,t;
+    cin>>t;
+    rep(i,t)
+        cin>>r[i]; /// Reminder
+    rep(i,t)
+        cin>>p[i]; /// Prime
+    rep(i,t)
     {
-        m=M/p[i];
-        extgcd(m,p[i],d,x,y);
-        ans=(ans+r[i]*m*x)%M;
+        a[i]=r[i];
+        n[i]=p[i];
+        //normalize(a[i], n[i]);
     }
-    return (ans+M)%M;
+    ans=a[1];
+    lcm=n[1];
+    for(i=2;i<=t;i++)
+    {
+        auto pom=ex_GCD(lcm,n[i]);
+        ll x1=pom.x;
+        ll d=pom.d;
+        if((a[i]-ans)%d!=0)
+        {
+            cout<<"Not Possible";
+            return 0;
+        }
+        ans=normalize(ans + x1 * (a[i] - ans) / d % (n[i] / d) * lcm, lcm * n[i] / d);
+        lcm=LCM(lcm, n[i]);
+    }
+    cout<<ans;
 }
